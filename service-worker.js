43397655cde8a0f4a6b61fe97bfe6d853bbfc2f7
Ziran Sun <ziran.sun@samsung.com>
2018-06-28 15:38:14 +0100

    Change cache query.

--- service-worker.js
+++ service-worker.js
@@ -81,7 +81,7 @@ self.addEventListener('fetch', function(e) {
      */
     e.respondWith(
       caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
+        return response;
       })
     );
   }
